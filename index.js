394612f0d389e7e54619448c5338f54c16146ce5
Marc0-Leal <marc.leals@duocuc.cl>
2026-04-27 11:48:18 -0400

    feat: add task command

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // Agrega esto en index.js
 const { readTasks, writeTasks } = require('./tasks');
-const [,, cmd] = process.argv;
+const [, , cmd] = process.argv;
+const title = process.argv.slice(3).join(' ');
 
 if (cmd === 'list') {
-  const tasks = readTasks();
-  if (tasks.length === 0) {
-    console.log('No hay tareas.');
-  } else {
-    tasks.forEach((t, i) => {
-      const icon = t.done ? '✓' : 'o';
-      console.log(`${icon} [${i + 1}] ${t.title}`);
-    });
-  }
+    const tasks = readTasks();
+    if (tasks.length === 0) {
+        console.log('No hay tareas.');
+    } else {
+        tasks.forEach((t, i) => {
+            const icon = t.done ? '✓' : 'o';
+            console.log(`${icon} [${i + 1}] ${t.title}`);
+        });
+    }
+    if (cmd === 'add') {
+        if (!title) {
+            console.log('Uso: node index.js add <título>');
+            process.exit(1);
+        }
+        const tasks = readTasks();
+        tasks.push({ id: Date.now(), title, done: false });
+        writeTasks(tasks);
+        console.log(`✓ Tarea "${title}" agregada.`);
+    }
 }
