e53c8cee101f1bd8a2843dfa61997a9c58eb25d9
Marc0-Leal <marc.leals@duocuc.cl>
2026-04-27 11:37:10 -0400

    feat: add list command

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// index.js
-const { readTasks } = require('./tasks');
+// Agrega esto en index.js
+const { readTasks, writeTasks } = require('./tasks');
 const [,, cmd] = process.argv;
 
-if (!cmd) {
-  console.log('TaskMaster CLI - usa: node index.js <comando>');
+if (cmd === 'list') {
+  const tasks = readTasks();
+  if (tasks.length === 0) {
+    console.log('No hay tareas.');
+  } else {
+    tasks.forEach((t, i) => {
+      const icon = t.done ? '✓' : 'o';
+      console.log(`${icon} [${i + 1}] ${t.title}`);
+    });
+  }
 }
